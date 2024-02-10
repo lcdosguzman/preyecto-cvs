f7b2b1681df606d3f2d1533913a9d5d23b1ec6e1
Simon Guzman <lcdosguzman@gmail.com>
2024-02-10 15:34:44 -0300

    se agregan los datos al index experiencia y educacion

--- proyectocvs/AppCurriculum/views.py
+++ proyectocvs/AppCurriculum/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from AppCurriculum.models import *
 
 # Create your views here.
 def home(request):
-    return render(request,"AppCurriculum/home.html")
+    experiencias = ExperienciaLaboral.objects.all()
+    estudios = Educacion.objects.all()
+    return render(request,"AppCurriculum/home.html",{"experiencias":experiencias,"estudios":estudios})
 
 def experiencia(request):
-    return render(request,"AppCurriculum/experiencia.html")
+    experiencias = ExperienciaLaboral.objects.all()
+    return render(request,"AppCurriculum/experiencia.html",{"experiencias":experiencias})
