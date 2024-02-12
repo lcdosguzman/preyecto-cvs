bf2f3578b335cf9e8c9242acde1ba09ed39a138b
Simon Guzman <lcdosguzman@gmail.com>
2024-02-12 13:42:10 -0300

    fix css formularios

--- proyectocvs/AppCurriculum/forms.py
+++ proyectocvs/AppCurriculum/forms.py
@@ -3,10 +3,10 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Submit
 
 class ExperienciaFormulario(forms.Form):
-    cargo = forms.CharField(label='Cargo', max_length=100)
-    empresa = forms.CharField(label='Empresa', max_length=100)
-    periodo_inicio = forms.DateField(label='Fecha Inicio')
-    periodo_fin = forms.DateField(label='Fecha Fin')
+    cargo = forms.CharField(label='Cargo', max_length=100,widget=forms.TextInput(attrs={'class': 'form-control'}))
+    empresa = forms.CharField(label='Empresa', max_length=100,widget=forms.TextInput(attrs={'class': 'form-control'}))
+    periodo_inicio = forms.DateField(label='Fecha Inicio',widget=forms.TextInput(attrs={'class': 'form-control'}))
+    periodo_fin = forms.DateField(label='Fecha Fin',widget=forms.TextInput(attrs={'class': 'form-control'}))
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper(self)
@@ -19,10 +19,10 @@ class ExperienciaFormulario(forms.Form):
         )
 
 class EstudioFormulario(forms.Form):
-    institucion = forms.CharField(label='Institucion', max_length=100)
-    titulo = forms.CharField(label='Titulo', max_length=100)
-    periodo_inicio = forms.DateField(label='Fecha Inicio')
-    periodo_fin = forms.DateField(label='Fecha Fin')
+    institucion = forms.CharField(label='Institucion', max_length=100,widget=forms.TextInput(attrs={'class': 'form-control'}))
+    titulo = forms.CharField(label='Titulo', max_length=100,widget=forms.TextInput(attrs={'class': 'form-control'}))
+    periodo_inicio = forms.DateField(label='Fecha Inicio',widget=forms.TextInput(attrs={'class': 'form-control'}))
+    periodo_fin = forms.DateField(label='Fecha Fin',widget=forms.TextInput(attrs={'class': 'form-control'}))
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper(self)
